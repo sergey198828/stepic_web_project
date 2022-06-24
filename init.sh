73606cebffaa906bf5261c028504e387209b9d83
Kiyan, Sergey <sergey.kiyan@effem.com>
2022-06-24 17:43:21 +0300

    Added init script and nginx.conf

---
+++ init.sh
@@ -0,0 +1,9 @@
+mkdir uploads
+mkdir -p public/{css,img,js}
+
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
