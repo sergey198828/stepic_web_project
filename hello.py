e9307abc5ebe84c13413c5e7110a3aafbb0d5985
kiyanser <sergey198828@gmail.com>
2022-06-25 19:33:31 +0300

    Fixed line ending

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ def wsgi_application(environ, start_responce):
     headers = [
         ("Content-Type", "text/plain")
     ]
-    body = environ['QUERY_STRING'].split("&")
+    body = [
+        (p + "\n").encode("utf-8") for p in environ['QUERY_STRING'].split("&")
+    ]
     start_responce(status, headers)
     return body
